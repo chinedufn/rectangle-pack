d3ed3c1034c4e0d4d4ca2b15f265921669576a9b
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2020-05-13 18:13:12 -0400

    Copy/clone for PackedLocation

--- src/packed_location.rs
+++ src/packed_location.rs
@@ -1,7 +1,7 @@
 use crate::width_height_depth::WidthHeightDepth;
 
 /// Describes how and where an incoming rectangle was packed into the target bins
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Copy, Clone)]
 pub struct PackedLocation {
     pub(crate) x: u32,
     pub(crate) y: u32,
@@ -12,7 +12,7 @@ pub struct PackedLocation {
     pub(crate) z_axis_rotation: RotatedBy,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Copy, Clone)]
 #[allow(unused)] // TODO: Implement rotations
 pub enum RotatedBy {
     ZeroDegrees,
